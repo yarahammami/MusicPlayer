1b815cf3b645b960ebbfcd1d52fe190228578884
yara hammami <yara.hammami13@gmail.com>
2023-02-06 14:48:42 +0200

    songs and songAdapter java class adittion

---
+++ app/src/main/java/com/example/musicplayer/songAdapter.java
@@ -0,0 +1,98 @@
+package com.example.musicplayer;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+
+public class songAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    //members
+    Context context;
+    List<Song> songs;
+
+    //constructors
+
+
+    public songAdapter(Context context, List<Song> songs) {
+        this.context = context;
+        this.songs = songs;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //inflate song row layout
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.song_row_item, parent,false);
+        return new SongViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    //current song and view holder
+        Song song = songs.get(position);
+        SongViewHolder viewHolder = (SongViewHolder) holder;
+
+        //set values to views
+        viewHolder.titleHolder.setText(song.getTitle());
+        viewHolder.durationHolder.setText(String.valueOf(song.getDuration()));
+        viewHolder.sizeHolder.setText(String.valueOf(song.getSize()));
+
+        //artwork
+        Uri artworkUri = song.getArtworkUri();
+        if(artworkUri != null){
+            //set the uri to image view
+            viewHolder.artworkHolder.setImageURI(artworkUri);
+
+            //make sure the uri has an artwork
+            if(viewHolder.artworkHolder.getDrawable() == null){
+                viewHolder.artworkHolder.setImageResource(R.drawable.default_artwork);
+            }
+
+        }
+
+        //on item click
+        viewHolder.itemView.setOnClickListener(view -> Toast.makeText(context, song.getTitle(), Toast.LENGTH_SHORT).show());
+
+    }
+
+    //view holder
+    public static class SongViewHolder extends RecyclerView.ViewHolder{
+        //members
+        ImageView artworkHolder;
+        TextView titleHolder, durationHolder,sizeHolder;
+
+        public SongViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            artworkHolder = itemView.findViewById(R.id.artWorkView);
+            titleHolder = itemView.findViewById(R.id.titleView);
+            durationHolder = itemView.findViewById(R.id.durationView);
+            sizeHolder = itemView.findViewById(R.id.sizeView);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return songs.size();
+    }
+
+//filter songs/ search results
+    @SuppressLint("NotifyDataSetChanged")
+    public void filterSongs(List<Song> filteredlist){
+        songs = filteredlist;
+        notifyDataSetChanged();
+    }
+    
+
+
+
+}
